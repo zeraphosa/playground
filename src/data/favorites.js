73f17b946388b1d5ebbe84508cf3fcec9ed14518
Zaur Sharifov <zaursharifov@outlook.com>
2022-09-08 21:52:35 +0300

    add favorites

---
+++ src/data/favorites.js
@@ -0,0 +1,26 @@
+export default [
+  {
+    id: 1,
+    title: "Pop",
+    cover: "https://t.scdn.co/images/0a74d96e091a495bb09c0d83210910c3",
+    color: "#79559f",
+  },
+  {
+    id: 2,
+    title: "Hip Hop",
+    cover: "https://t.scdn.co/images/d0fb2ab104dc4846bdc56d72b0b0d785.jpeg",
+    color: "#ba5d00",
+  },
+  {
+    id: 3,
+    title: "Rock",
+    cover: "https://charts-images.scdn.co/assets/locale_en/regional/weekly/region_global_default.jpg",
+    color: "#e51d32",
+  },
+  {
+    id: 4,
+    title: "Dans ve Elektronik",
+    cover: "https://i.scdn.co/image/ab67706f000000027ea4d505212b9de1f72c5112",
+    color: "#dc148b",
+  },
+];
--- src/views/Search.js
+++ src/views/Search.js
@@ -1,4 +1,8 @@
+import Title from "components/Title";
 import categories from "data/categories";
+import favorites from "data/favorites";
+import { useEffect, useRef } from "react";
+import ScrollContainer from "react-indiana-drag-scroll";
 
 function Category({ category }) {
   return (
@@ -11,12 +15,43 @@ function Category({ category }) {
   );
 }
 
-export default function Search() {
+function WideCategory({ favorite }) {
   return (
-    <div className="grid grid-cols-5 gap-6">
-      {categories.map((category) => (
-        <Category category={category} />
-      ))}
+    <div style={{ background: favorite.color }} className="relative rounded-lg w-[27.375rem] h-[13.75rem] flex-shrink-0">
+      <div className="absolute inset-0 overflow-hidden">
+        <h3 className="p-4 text-[2.5rem] tracking-tighter font-semibold">{favorite.title}</h3>
+        <img src={favorite.cover} alt="" className="shadow-spotify w-32 h-32 rotate-[25deg] translate-x-[18%] translate-y-5%] absolute bottom-0 right-0" />
+      </div>
     </div>
+  );
+}
+
+export default function Search() {
+  const favoritesRef = useRef();
+  useEffect(() => {
+    if (favoritesRef.current) {
+      console.log(favoritesRef.current.container.current.scrollWidth);
+    }
+  }, [favoritesRef]);
+  return (
+    <>
+      <section className="mb-4">
+        <Title title={"En çok dinlediğin türler"} />
+        <ScrollContainer className="flex overflow-x gap-x-6 scrollable" ref={favorites}>
+          {favorites.map((favorite, index) => (
+            <WideCategory key={index} favorite={favorite} />
+          ))}
+        </ScrollContainer>
+      </section>
+
+      <section>
+        <Title title={"Hepsine göz at"} />
+        <div className="grid grid-cols-5 gap-6">
+          {categories.map((category, index) => (
+            <Category key={index} category={category} />
+          ))}
+        </div>
+      </section>
+    </>
   );
 }
