929c942f7837fb731d4f088b3f81fb58e915544a
Zaur Sharifov <zaursharifov@outlook.com>
2022-09-20 18:56:43 +0300

    react add

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,37 @@
 import express from "express";
+import mysql from "mysql";
 
-const app = express();
 const port = 8800;
+const app = express();
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "Ze2871781O.!",
+  database: "test",
+});
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json("hi from backend");
+});
+app.get("/books", (req, res) => {
+  const q = "SELECT * FROM books";
+  db.query(q, (err, data) => {
+    if (err) return res.json(err);
+    return res.json(data);
+  });
+});
+
+app.post("/books", (req, res) => {
+  const q = "INSERT INTO books (`title`,`desc`,`cover`) VALUES (?)";
+  const values = [req.body.title, req.body.desc, req.body.cover];
+  db.query(q, [values], (err, data) => {
+    if (err) return res.json(err);
+    return res.json("Book has been created successfully");
+  });
+});
+
 app.listen(port, () => {
-  console.log("Connected to backend!!121");
+  console.log("Connected to backend!");
 });
