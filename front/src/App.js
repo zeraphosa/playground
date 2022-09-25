7b4c62a12e701337046af27d44024aea4fc330d4
Zaur Sharifov <zaursharifov@outlook.com>
2022-09-25 23:37:20 +0300

    sidebar names

--- front/src/App.js
+++ front/src/App.js
@@ -4,13 +4,6 @@ import { BrowserRouter as Router } from "react-router-dom";
 
 export default function App() {
   const [open, set_open] = useState(true);
-  const pages = [
-    { title: "Tasks", name: "tasks" },
-    { title: "Research", name: "researches" },
-    { title: "Accounts", name: "accounts", gap: true },
-    { title: "Projects", name: "projects" },
-    { title: "WatchList", name: "watchlist", gap: true },
-  ];
   return (
     <Router>
       <div className="flex bg-gray-800">
@@ -18,15 +11,38 @@ export default function App() {
           <div className="flex items-center justify-center absolute cursor-pointer -right-3 top-9 w-7 h-7 bg-primary rounded-full" onClick={() => set_open(!open)}>
             {open ? <Icon size={20} name="prev" /> : <Icon size={20} name="next" />}
           </div>
-
-          <div className="flex gap-x-4 items-center">
-            <div className={`cursor-pointer duration-500`}>
-              <Icon name="tasks" />
+          <div className="flex flex-col gap-y-6">
+            <div className="flex gap-x-4 items-center">
+              <div className={`cursor-pointer duration-500`}>
+                <Icon name="tasks" />
+              </div>
+              <h1 className={`text-white origin-left font-medium text-xl duration-300 ${!open && "scale-0"}`}>Tasks</h1>
             </div>
-            <h1 className={`text-white origin-left font-medium text-xl duration-300 ${!open && "scale-0"}`}>Tasks</h1>
+            <div className="flex gap-x-4 items-center">
+              <div className={`cursor-pointer duration-500`}>
+                <Icon name="notes" />
+              </div>
+              <h1 className={`text-white origin-left font-medium text-xl duration-300 ${!open && "scale-0"}`}>Notes</h1>
+            </div>
+            <div className="flex gap-x-4 items-center">
+              <div className={`cursor-pointer duration-500`}>
+                <Icon name="accounts" />
+              </div>
+              <h1 className={`text-white origin-left font-medium text-xl duration-300 ${!open && "scale-0"}`}>Accounts</h1>
+            </div>
+            <div className="flex gap-x-4 items-center">
+              <div className={`cursor-pointer duration-500`}>
+                <Icon name="projects" />
+              </div>
+              <h1 className={`text-white origin-left font-medium text-xl duration-300 ${!open && "scale-0"}`}>Projects</h1>
+            </div>
+            <div className="flex gap-x-4 items-center">
+              <div className={`cursor-pointer duration-500`}>
+                <Icon name="watchlist" />
+              </div>
+              <h1 className={`text-white origin-left font-medium text-xl duration-300 ${!open && "scale-0"}`}>WatchList</h1>
+            </div>
           </div>
-
-      
         </div>
         <div className="p-7 text-2xl font-semibold flex-1 h-screen">Main page</div>
       </div>
