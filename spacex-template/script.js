58f73286eaab57bbf41084f7ef7b1b1df18f8e05
Zaur Sharifov <zaursharifov@outlook.com>
2022-10-04 18:45:58 +0300

    spacex - side menu add

---
+++ spacex-template/script.js
@@ -0,0 +1,11 @@
+const btn = document.getElementById("menu-btn");
+const overlay = document.getElementById("overlay");
+const menu = document.getElementById("mobile-menu");
+
+btn.addEventListener("click", navToggle);
+function navToggle() {
+  btn.classList.toggle("open");
+  overlay.classList.toggle("overlay-show");
+  document.body.classList.toggle("stop-scroll");
+  menu.classList.toggle("show-menu");
+}
