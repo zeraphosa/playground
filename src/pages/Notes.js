68cb5c65ef9d8fd30d6b41f21cf2b1b9ab45b183
Zaur Sharifov <zaursharifov@outlook.com>
2022-10-18 00:02:06 +0300

    whacthlist start

--- src/pages/Notes.js
+++ src/pages/Notes.js
@@ -1,3 +1,11 @@
 export default function Notes() {
-  return <div>Notes</div>;
+  return (
+    <div className="notes">
+      <div className="notes_form">
+        <input type="text" placeholder="new task" className="input" />
+        <button className="button">+</button>
+      </div>
+      <div className="notes_list"></div>
+    </div>
+  );
 }
--- src/pages/Watchlist.js
+++ src/pages/Watchlist.js
@@ -1,3 +1,44 @@
 export default function Watchlist() {
-  return <div>Watchlist</div>;
+  return (
+    <div className="watchlist">
+      <div className="wl_form">
+        <input type="text" placeholder="name" className="input" />
+        <input type="text" placeholder="type" className="input" />
+        <input type="text" placeholder="status" className="input" />
+        <button className="button">+</button>
+      </div>
+      <table>
+        <th>
+          <tr>Name</tr>
+          <tr>Type</tr>
+          <tr>Status</tr>
+        </th>
+        {data.map((item, idx) => (
+          <td>
+            <tr>{item.name}</tr>
+            <tr>{item.type}</tr>
+            <tr>{item.status}</tr>
+          </td>
+        ))}
+      </table>
+    </div>
+  );
 }
+
+const data = [
+  {
+    name: "Bates Motel",
+    type: "serie",
+    status: "half",
+  },
+  {
+    name: "TWD",
+    type: "serie",
+    status: "watched",
+  },
+  {
+    name: "Wanted",
+    type: "film",
+    status: "watched",
+  },
+];
