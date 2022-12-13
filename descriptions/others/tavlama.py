47bf20a31a34c8e034116172457fd525b6bd5021
Zaur Sharifov <zaursharifov@outlook.com>
2022-12-13 16:33:34 +0300

    tavlama -> app func done

--- descriptions/others/tavlama.py
+++ descriptions/others/tavlama.py
@@ -25,3 +25,38 @@ def app(alt_sinir,ust_sinir,d,delta,T,Tend,sk):
 
     objeniyi = obj #son
     cozumeniyi = cozum #son
+
+    while Tend < T:
+        degisim_miktari = get_DegisimMiktari(-(ust_sinir - alt_sinir)*delta/2, (ust_sinir - alt_sinir)*delta/2,d)
+        cozum =get_Cozum(d)
+        komsu = cozum + degisim_miktari
+        obj_komsu = get_Obj(komsu)
+
+        if(obj_komsu < obj):
+            obj = obj_komsu
+        else: # Probability of acceptance
+            # amac fonksiyonunun ne kadar kotulestigini olcer pa: p(deltaE,T)= e ussu (-deltaE/T)
+            de = obj_komsu - obj
+            pa = np.exp(-de/T)
+            rs = np.random.uniform(0,1)
+
+            if(rs<pa):
+                cozum = komsu
+                obj = obj_komsu
+        T = T*sk
+        iterasyon = iterasyon + 1
+
+        if(obj< np.min(array_obj)):
+            array_obj.append(obj)
+            array_iterasyon.append(iterasyon)
+
+        if(obj<objeniyi):
+            objeniyi = obj
+            cozumeniyi=cozum
+    return cozum, obj, iterasyon, array_obj,array_iterasyon
+
+cozum, obj, iterasyon,array_obj,array_iterasyon = app(-5,5,6,0.1,1000,0.1,0.9)
+
+print("cozum -> ",cozum)
+print("iterasyon -> ", iterasyon)
+print("obj -> ",obj)
