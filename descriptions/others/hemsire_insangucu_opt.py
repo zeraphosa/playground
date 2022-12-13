e001780834d7d9101d2835f89ec97b1b2b6a9188
Zaur Sharifov <zaursharifov@outlook.com>
2022-12-13 20:21:59 +0300

    nurse.py -> ceza hesapla func

--- descriptions/others/hemsire_insangucu_opt.py
+++ descriptions/others/hemsire_insangucu_opt.py
@@ -30,3 +30,42 @@ def haftalik_calisma(nsp_tablo):
             haftalik_calisma = nsp_tablo[p,i].sum()
             nsp_tablo[p,i,-1] = haftalik_calisma
     return nsp_tablo
+
+# ceza durumunu hesapla
+def ceza_hesapla(nsp_tablo):
+    kd_sabah = 8.45
+    kd_aksam = 15.75
+    ceza_tablo = np.zeros((populasyon,1))
+    
+    for p in range(0,nsp_tablo.shape[0]):
+        ceza = 0
+        for i in range(0,nsp_tablo.shape[1]):
+            if nsp_tablo[p,i,-1] != calisma_saati:  # haftalık çalışma saati
+                ekle = nsp_tablo[p,i,-1] - calisma_saati
+                if ekle < 0:
+                    ekle = -1* ekle
+                ceza += ekle
+       
+        for i in range(0,nsp_tablo.shape[2]-1):
+            sabah_calisma = 0
+            aksam_calisma = 0
+            for j in range(0,nsp_tablo.shape[1]):
+                if nsp_tablo[p,j,i] == 8:
+                    sabah_calisma += 1
+                elif nsp_tablo[p,j,i] == 16:
+                    aksam_calisma += 2
+            hastabakim_sabah = sabah_calisma * 4.64    #4.64 günlük hemsire ilgilenme süresi
+            hastabakim_aksam = aksam_calisma * 4.64
+            
+            ceza_hBs = hastabakim_sabah - kd_sabah
+            ceza_hBa = hastabakim_aksam - kd_aksam
+            
+            if ceza_hBs < 0:
+                ceza_hBs = -1 * ceza_hBs
+            if ceza_hBa < 0:
+                ceza_hBa = -1 * ceza_hBa   
+                
+            ceza += ceza_hBs + ceza_hBa
+        ceza_tablo[p] = ceza
+        
+    return ceza_tablo
