cc08a9f434e635cb7759fcf984a77966ccf72dc2
Zaur Sharifov <zaursharifov@outlook.com>
2022-12-13 16:28:00 +0300

    app func

--- descriptions/others/tavlama.py
+++ descriptions/others/tavlama.py
@@ -12,3 +12,16 @@ def get_Obj(cozum):
 def get_DegisimMiktari(alt_sinir, ust_sinir, d):
     degisimMiktari = np.random.uniform(alt_sinir, ust_sinir,d)
     return degisimMiktari
+
+def app(alt_sinir,ust_sinir,d,delta,T,Tend,sk):
+    array_obj=[]
+    array_iterasyon=[]
+    iterasyon = 1
+    array_iterasyon.append(iterasyon)
+    cozum = get_Cozum(d)
+
+    obj= get_Obj(cozum)
+    array_obj.append(obj)
+
+    objeniyi = obj #son
+    cozumeniyi = cozum #son
