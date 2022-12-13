d0bbfb4de2259f45fdbfa9390fbf87522ee85dc1
Zaur Sharifov <zaursharifov@outlook.com>
2022-12-13 19:27:57 +0300

    nurse.py -> gece func

--- descriptions/others/hemsire_insangucu_opt.py
+++ descriptions/others/hemsire_insangucu_opt.py
@@ -11,3 +11,14 @@ if(gun_sayisi == 7):
 else:
     calisma_saati = 160
 
+# 1. sinirlilik: gece vardiyasindan sonraki gun calisilmayacak
+def gece_vardiyası(NSPtablo):
+    tablo = NSPtablo.copy()
+    for p in range(0,tablo.shape[0]):  #populasyondaki her bir diziye erişim
+        for i in range(0,tablo.shape[1]):  # hemsire sayısı
+            for j in range(1,tablo.shape[2]): # gün sayısı
+                if tablo[p, i,j-1] == 16:
+                    tablo[p, i,j] = 0
+            
+    return tablo
+
