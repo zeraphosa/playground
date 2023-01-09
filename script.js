b4758b82593aa31d82de0f0317ab0125a1c9d21f
Zaur Sharifov <zaursharifov@outlook.com>
2023-01-09 17:48:24 +0300

    index data add

--- script.js
+++ script.js
@@ -9,132 +9,158 @@ const data = [
     n: "Portfolio Example 2",
     i: "./assets/projects/portfolio-example-2.png",
     c: "templates",
+    l: "./projects/portfolio-template-0/index.html"
   },
 
   {
     n: "Buta Assignment 1",
     i: "./assets/projects/buta-assignment-1.png",
     c: "templates",
+    l: "./projects/buta-assignment-1/index.html"
   },
   {
     n: "Buta Assignment 2",
     i: "./assets/projects/buta-assignment-2.png",
     c: "templates",
+    l: "./projects/buta-assignment-2/index.html"
   },
   {
     n: "Buta Assignment 3",
     i: "./assets/projects/buta-assignment-3.png",
     c: "templates",
+    l: "./projects/buta-assignment-3/index.html"
   },
   {
     n: "Blog Template",
     i: "./assets/projects/blog-template.png",
     c: "templates",
+    l: "./projects/blog-template/index.html"
   },
   {
     n: "Blurry Loading",
     i: "./assets/projects/blurry-loading.png",
     c: "others",
+    l: "./projects/blurry-loading/index.html"
   },
   {
     n: "Calculator",
     i: "./assets/projects/calculator.png",
     c: "others",
+    l: "./projects/calculator.html"
   },
   {
     n: "CityTab",
     i: "./assets/projects/citytab.png",
     c: "others",
+    l: "./projects/citytab.html"
   },
   {
     n: "Countdown",
     i: "./assets/projects/countdown.png",
     c: "others",
+    l: "./projects/countdown.html"
   },
   {
     n: "Expanding Cards",
     i: "./assets/projects/expanding-cards.png",
     c: "others",
+    l: "./projects/expanding-cards.html"
   },
   {
     n: "Express Template",
     i: "./assets/projects/express-template.png",
     c: "templates",
+    l: "./projects/express-template/index.html"
   },
   {
     n: "Form Wave Animation",
     i: "./assets/projects/form-wave-animation.png",
     c: "others",
+    l: "./projects/form-wave-animation.html"
   },
   {
     n: "Hidden Search Widget",
     i: "./assets/projects/hidden-search-widget.png",
     c: "others",
+    l: "./projects/hidden-search-widget.html"
   },
   {
     n: "Progress Steps",
     i: "./assets/projects/progress-steps.png",
     c: "others",
+    l: "./projects/progress-steps.html"
   },
   {
     n: "Quiz App",
     i: "./assets/projects/quiz-app.png",
     c: "others",
+    l: "./projects/quiz-app/index.html"
   },
   {
     n: "Random Password",
     i: "./assets/projects/random-password.png",
     c: "others",
+    l: "./projects/random-password.html"
   },
   {
     n: "Random Photos",
     i: "./assets/projects/random-photos.png",
     c: "others",
+    l: "./projects/random-photos.html"
   },
   {
     n: "Rotating Navigation",
     i: "./assets/projects/rotating-navigation.png",
     c: "others",
+    l: "./projects/rotating-navigation.html"
   },
   {
     n: "Scroll Snaps",
     i: "./assets/projects/scroll-snaps.png",
     c: "others",
+    l: "./projects/scroll-snaps.html"
   },
   {
     n: "Sound Board",
     i: "./assets/projects/sound-board.png",
     c: "others",
+    l: "./projects/sound-board/index.html"
   },
   {
     n: "SpaceX Template",
     i: "./assets/projects/spacex-template.png",
     c: "templates",
+    l: "./projects/spacex-template/index.html"
   },
   {
     n: "Split Landing Page",
     i: "./assets/projects/split-landing-page.png",
     c: "others",
+    l: "./projects/split-landing-page/index.html"
   },
   {
     n: "Statistic Counter",
     i: "./assets/projects/statistic-counter.png",
     c: "others",
+    l: "./projects/statistic-counter.html"
   },
   {
     n: "Todo App",
     i: "./assets/projects/todo-app.png",
     c: "others",
+    l: "./projects/todo-app.html"
   },
   {
     n: "Verify Digit",
     i: "./assets/projects/verify-digit.png",
     c: "others",
+    l: "./projects/verify-digit.html"
   },
   {
     n: "Video Popup",
     i: "./assets/projects/video-popup.png",
     c: "others",
+    l: "./projects/video-popup/index.html"
   },
 ];
 
