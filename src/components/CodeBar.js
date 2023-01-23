2d346158d89a66654b9b79f3781db169b6613037
Zaur Sharifov <zaursharifov@outlook.com>
2023-01-23 22:59:48 +0300

    ace editor add

--- src/components/CodeBar.js
+++ src/components/CodeBar.js
@@ -1,5 +1,11 @@
 import React, { useState } from "react";
 import styles from "./CodeBar.module.css";
+import AceEditor from "react-ace";
+import "ace-builds/src-noconflict/mode-html";
+import "ace-builds/src-noconflict/theme-monokai";
+import "ace-builds/src-noconflict/snippets/html";
+import "ace-builds/src-min-noconflict/ext-searchbox";
+import "ace-builds/src-min-noconflict/ext-language_tools";
 
 export default function CodeBar() {
   const [activeTab, setActiveTab] = useState("html");
@@ -9,6 +15,25 @@ export default function CodeBar() {
         <button onClick={()=> setActiveTab('css')} className={`${styles.item} ${activeTab === 'css' && styles.activeTab}`}>CSS</button>
         <button onClick={()=> setActiveTab('js')} className={`${styles.item} ${activeTab === 'js' && styles.activeTab}`}>JS</button>
     </nav>
+
+    <AceEditor
+        placeholder="Write your HTML codes here!"
+        mode="html"
+        theme="monokai"
+        name="editor_html"
+        fontSize={14}
+        showPrintMargin={true}
+        showGutter={true}
+        highlightActiveLine={true}
+        editorProps={{ $blockScrolling: true }} 
+        setOptions={{
+            enableBasicAutocompletion: true,
+            enableLiveAutocompletion: true,
+            enableSnippets: true,
+            showLineNumbers: true,
+            tabSize: 2,
+        }}
+    />
 
   </aside>;
 }
