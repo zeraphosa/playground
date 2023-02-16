4e197d428b265dab05cc29baea788b8b1359d206
Zaur Sharifov <zaursharifov@outlook.com>
2023-02-16 07:04:51 +0300

    deploy test - react side done

--- fullstack-deploy-example/src/App.js
+++ fullstack-deploy-example/src/App.js
@@ -1,9 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 export default function App() {
+  const url = "http://localhost:5600/";
+  const [data, setData] = useState([]);
+  
+  useEffect(()=>{
+    async function fetchData(){
+      try{
+        const res = await axios.get(`${url}`);
+        setData(res.data);
+      }catch(err){
+        console.log("gelmir:",err);
+      }
+    }
+    fetchData();
+  },[]);
+
   return (
-    <div className="App">
-      APP
+    <div>
+      {data.map((item)=>(
+        <div key={item.id}>{item.text}</div>
+      ))}
     </div>
   );
 }
