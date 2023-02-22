7c5d373d348554a2634285350a5f5810ab3d1e4f
Zaur Sharifov <zaursharifov@outlook.com>
2023-02-23 01:20:53 +0300

    test without db

--- index.js
+++ index.js
@@ -1,27 +1,27 @@
 const express = require("express");
-const mysql = require("mysql");
-const cors = require("cors");
+// const mysql = require("mysql");
+// const cors = require("cors");
 
-const port = process.env.PORT || 5600;
 const app = express();
-const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "Ze2871781O.!",
-    database: "deploy-test",
-});
+// const db = mysql.createConnection({
+//     host: "localhost",
+//     user: "root",
+//     password: "Ze2871781O.!",
+//     database: "deploy-test",
+// });
 
 app.use(express.json());
-app.use(cors());
+// app.use(cors());
 
-app.get("/", (req,res)=>{
-    const q = "SELECT * FROM deneme";
-    db.query(q,(err,data)=>{
-        if(err) return res.json(err);
-        return res.json(data);
-    })
-})
+app.get("/", (req, res) => {
+  res.send("Working");
+  // const q = "SELECT * FROM deneme";
+  // db.query(q,(err,data)=>{
+  //     if(err) return res.json(err);
+  //     return res.json(data);
+  // })
+});
 
-app.listen(port, ()=>{
-    console.log("CONNECTED SERVER");
-})
+app.listen(process.env.PORT || 5600, () => {
+  console.log("CONNECTED SERVER");
+});
