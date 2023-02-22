85c354f601aef69eeb27d63f3b5bd2a6d0b2d8a6
Zaur Sharifov <zaursharifov@outlook.com>
2023-02-23 01:27:00 +0300

    env file created

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
 const express = require("express");
-// const mysql = require("mysql");
-// const cors = require("cors");
+const mysql = require("mysql");
+const cors = require("cors");
 
 const app = express();
-// const db = mysql.createConnection({
-//     host: "localhost",
-//     user: "root",
-//     password: "Ze2871781O.!",
-//     database: "deploy-test",
-// });
+
+const db = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
 
 app.use(express.json());
-// app.use(cors());
+app.use(cors());
 
 app.get("/", (req, res) => {
-  res.send("Working");
-  // const q = "SELECT * FROM deneme";
-  // db.query(q,(err,data)=>{
-  //     if(err) return res.json(err);
-  //     return res.json(data);
-  // })
+  const q = "SELECT * FROM deneme";
+  db.query(q, (err, data) => {
+    if (err) return res.json(err);
+    return res.json(data);
+  });
 });
 
 app.listen(process.env.PORT || 5600, () => {
