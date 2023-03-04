06b6c5368ee677d92ca3e6a1748bdef092a754f9
Zaur Sharifov <zaursharifov@outlook.com>
2023-03-04 20:01:39 +0400

    div cut

--- exercise-react/src/App.js
+++ exercise-react/src/App.js
@@ -1,5 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function App() {
-  return <div>app</div>;
+  const [leftElement, setLeftElement] = useState(false);
+  const [rightElement, setRightElement] = useState(false);
+
+  function handleMouseMove(event) {
+    const { clientX } = event;
+    const rect = event.currentTarget.getBoundingClientRect();
+    const halfWidth = rect.width / 2;
+    if (clientX >= rect.left && clientX <= rect.left + halfWidth) {
+      setLeftElement(true);
+      setRightElement(false);
+    } else if (clientX > rect.left + halfWidth && clientX <= rect.right) {
+      setLeftElement(false);
+      setRightElement(true);
+    } else {
+      setLeftElement(false);
+      setRightElement(false);
+    }
+  }
+
+  function handleMouseLeave() {
+    setLeftElement(false);
+    setRightElement(false);
+  }
+
+  return (
+    <div onMouseMove={handleMouseMove} onMouseLeave={handleMouseLeave} style={{ display: "flex" }}>
+      <div
+        style={{
+          width: "50%",
+          height: "100px",
+          backgroundColor: leftElement ? "red" : "blue",
+        }}
+      />
+      <div
+        style={{
+          width: "50%",
+          height: "100px",
+          backgroundColor: rightElement ? "red" : "blue",
+        }}
+      />
+    </div>
+  );
 }
