620cd021f866fcda7be9bcd54749d15b81903e4a
Zaur Sharifov <zaursharifov@outlook.com>
2023-03-22 11:52:15 +0400

    tailwind config default

--- example-tailwind/tailwind.config.js
+++ example-tailwind/tailwind.config.js
@@ -1,10 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './public/**/*.{html,js}',
-  ],
+  content: ["./public/**/*.{html,js}"],
   theme: {
-    extend: {},
+    colors: {
+      primary: "#ff6363",
+      secondary: {
+        100: "#e2e2d5",
+        200: "#888883",
+      },
+    },
   },
   plugins: [],
-}
+};
