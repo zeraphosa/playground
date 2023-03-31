1d138aec8c66cbfb9de44cde422492565df648e8
Zaur Sharifov <zaursharifov@outlook.com>
2023-03-31 14:46:36 +0400

    react-bootstrap example start

---
+++ example-bootstrap/src/App.js
@@ -0,0 +1,10 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import Button from "react-bootstrap/Button";
+
+export default function App() {
+  return (
+    <div>
+      <Button variant="primary">Primary</Button>
+    </div>
+  );
+}
