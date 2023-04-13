af022d7d826ef9e1c79b2a023b50afe470690dcd
Zaur Sharifov <zaursharifov@outlook.com>
2023-04-13 17:53:30 +0400

    appearance - text colors on theme change

--- redux-appearance/src/App.js
+++ redux-appearance/src/App.js
@@ -6,9 +6,16 @@ export default function App() {
 
   return (
     <div className="app">
-      <div className={`${theme}`}>Navbar</div>
-      <div>Sidebar</div>
-      <div>Content</div>
+      <div className={`navbar ${theme}`}>Navbar</div>
+      <div className={`sidebar ${theme}`}>
+        <div className="top">
+          <ul>
+            <li><a href="/">Home</a></li>
+            <li><a href="/">About</a></li>
+          </ul>
+        </div>
+        <div className="bottom">Bottom</div>
+      </div>
       <button onClick={toggleTheme}>Toggle Appearance</button>
     </div>
   );
