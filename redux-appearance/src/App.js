6575c7d56b4488cc52fd9860afee6f2c9afc3963
Zaur Sharifov <zaursharifov@outlook.com>
2023-04-13 17:45:17 +0400

    appearance - custom hook

--- redux-appearance/src/App.js
+++ redux-appearance/src/App.js
@@ -1,30 +1,15 @@
-import { useSelector, useDispatch } from "react-redux";
+import useDarkMode from "./hooks/useDarkMode";
 import "./App.css";
-import { useEffect, useState } from "react";
 
 export default function App() {
-  const dispatch = useDispatch();
-  const mode = useSelector((state) => state.mode.appearance);
-  const [theme, setTheme] = useState("light");
-
-  function handleClick() {
-    dispatch({ type: "setAppearance" });
-  }
-
-  useEffect(() => {
-    if (mode === true) {
-      setTheme("dark");
-    } else {
-      setTheme("light");
-    }
-  },[mode]);
+  const [theme, toggleTheme] = useDarkMode();
 
   return (
     <div className="app">
       <div className={`${theme}`}>Navbar</div>
       <div>Sidebar</div>
       <div>Content</div>
-      <button onClick={handleClick}>Toggle Appearance</button>
+      <button onClick={toggleTheme}>Toggle Appearance</button>
     </div>
   );
 }
