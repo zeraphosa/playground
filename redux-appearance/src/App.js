7d5a8e7a3c9e0909c782e07c83140ef60e0eff1a
Zaur Sharifov <zaursharifov@outlook.com>
2023-04-14 14:43:49 +0400

    i18 done

--- redux-appearance/src/App.js
+++ redux-appearance/src/App.js
@@ -2,19 +2,19 @@ import { useTranslation } from "react-i18next";
 
 export default function App() {
   const { t, i18n } = useTranslation();
-  function changeLanguage(e) {
-    i18n.changeLanguage(e.target.value);
-  }
+
+
   return (
     <div className="app">
-      <h1>{t("header")}</h1>
-      <button value="en" name="language" onClick={(e)=>changeLanguage(e)}>
+      <h1>{t('header')}</h1>
+      <p>{t('paragraph')}</p>
+      <button value="en" name="language" onClick={(e) => i18n.changeLanguage(e.target.value)}>
         English
       </button>
-      <button value="tr" name="language" onClick={(e)=>changeLanguage(e)}>
+      <button value="tr" name="language" onClick={(e) => i18n.changeLanguage(e.target.value)}>
         Turkish
       </button>
-      <button value="az" name="language" onClick={(e)=>changeLanguage(e)}>
+      <button value="az" name="language" onClick={(e) => i18n.changeLanguage(e.target.value)}>
         Azerbaijani
       </button>
     </div>
--- redux-appearance/src/index.js
+++ redux-appearance/src/index.js
@@ -1,10 +1,33 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import i18n from "i18next";
+import { I18nextProvider, initReactI18next } from "react-i18next";
+// import translations from "./translations.json";
+import enTranslation from "./en.json";
+import trTranslation from "./tr.json";
+
+i18n.use(initReactI18next).init({
+  resources: {
+    en: {
+      translation: enTranslation,
+    },
+    tr: {
+      translation: trTranslation,
+    },
+  },
+  lng: "en",
+  fallbackLng: "en",
+  interpolation: {
+    escapeValue: false,
+  },
+});
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <I18nextProvider i18n={i18n}>
+      <App />
+    </I18nextProvider>
   </React.StrictMode>,
 );
