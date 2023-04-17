bc8e2dc3c854c818a790c121c1579ac8dae7fd6c
Zaur Sharifov <zaursharifov@outlook.com>
2023-04-17 11:56:51 +0400

    currency converter example

--- redux-appearance/src/App.js
+++ redux-appearance/src/App.js
@@ -1,22 +1,55 @@
-import { useTranslation } from "react-i18next";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import Dropdown from "react-dropdown";
 
 export default function App() {
-  const { t, i18n } = useTranslation();
+  const [info, setInfo] = useState([]);
+  const [input, setInput] = useState(0);
+  const [from, setFrom] = useState("usd");
+  const [to, setTo] = useState("azn");
+  const [options, setOptions] = useState([]);
+  const [output, setOutput] = useState(0);
 
+  useEffect(() => {
+    axios.get(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${from}.json`).then((res) => {
+      setInfo(res.data[from]);
+    });
+  }, [from]);
+
+  useEffect(() => {
+    setOptions(Object.keys(info));
+    convert();
+  }, [info]);
+
+  function convert() {
+    var rate = info[to];
+    setOutput(input * rate);
+  }
 
   return (
     <div className="app">
-      <h1>{t('header')}</h1>
-      <p>{t('paragraph')}</p>
-      <button value="en" name="language" onClick={(e) => i18n.changeLanguage(e.target.value)}>
-        English
-      </button>
-      <button value="tr" name="language" onClick={(e) => i18n.changeLanguage(e.target.value)}>
-        Turkish
-      </button>
-      <button value="az" name="language" onClick={(e) => i18n.changeLanguage(e.target.value)}>
-        Azerbaijani
-      </button>
+      <input type="text" onChange={(e) => setInput(e.target.value)} />
+      <div>
+        <h3>From</h3>
+        <Dropdown options={options} onChange={(e) => setFrom(e.value)} value={from} placeholder="FROM" />
+      </div>
+      <div>
+        <h3>TO</h3>
+        <Dropdown options={options} onChange={(e) => setTo(e.value)} value={to} placeholder="TO" />
+      </div>
+
+      <div className="result">
+        <button
+          onClick={() => {
+            convert();
+          }}
+        >
+          Convert
+        </button>
+        <h2>Converted Amount:</h2>
+        <p>{input + " " + from + " = " + output.toFixed(2) + " " + to}</p>
+      </div>
+
     </div>
   );
 }
