6d6b206a922e69e631f7bf77a73e5070531f6b5f
Zaur Sharifov <zaursharifov@outlook.com>
2023-04-20 10:46:32 +0400

    clear

---
+++ redux-appearance/src/Sidebar.js
@@ -0,0 +1,11 @@
+export default function Sidebar(){
+    return(
+        <div>
+            <ul>
+                <li>project 1</li>
+                <li>project 2</li>
+                <li>project 3</li>
+            </ul>
+        </div>
+    )
+}
