2ccf6d1dd645d7535d9e59752a312bd3a86b5029
Zaur Sharifov <zaursharifov@outlook.com>
2023-04-29 18:58:42 +0400

    react-draft-wysiwyg example

--- example-redux/src/App.js
+++ example-redux/src/App.js
@@ -2,6 +2,8 @@ import React from "react";
 // import Input from "./components/Input";
 // import Todo from "./components/Todo";
 import "./style.css";
+import { Editor } from "react-draft-wysiwyg";
+import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
 
 export default function App() {
   return (
@@ -10,10 +12,16 @@ export default function App() {
       <Todo /> */}</div>
       <div className="post">
         <div className="header-container">
-          <div className="post-input">ded</div>
-          <div className="post-image">ad</div>
+          <div className="post-input"></div>
+          <div className="post-image"></div>
         </div>
-        <div className="article"></div>
+        <div className="article">
+        <Editor
+          toolbarClassName="toolbarClassName"
+          wrapperClassName="wrapperClassName"
+          editorClassName="editorClassName"
+        />
+        </div>
         <div className="save"></div>
       </div>
     </>
