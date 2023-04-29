79fd8547223d9b74c2c46adecdb7d22edbc39a23
Zaur Sharifov <zaursharifov@outlook.com>
2023-04-29 17:56:46 +0400

    react-rich-text-editor

--- example-redux/src/App.js
+++ example-redux/src/App.js
@@ -1,13 +1,21 @@
 import React from "react";
-import Input from "./components/Input";
-import Todo from "./components/Todo";
+// import Input from "./components/Input";
+// import Todo from "./components/Todo";
 import "./style.css";
 
 export default function App() {
   return (
-    <div >
-      <Input />
-      <Todo />
-    </div>
+    <>
+      <div>{/* <Input />
+      <Todo /> */}</div>
+      <div className="post">
+        <div className="header-container">
+          <div className="post-input">ded</div>
+          <div className="post-image">ad</div>
+        </div>
+        <div className="article"></div>
+        <div className="save"></div>
+      </div>
+    </>
   );
 }
