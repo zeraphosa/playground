91026098a27f40b8dcaf0152d12055064ed2a3cb
Zaur Sharifov <zaursharifov@outlook.com>
2023-05-09 14:44:10 +0400

    login-example server connected

--- example-redux/server/index.js
+++ example-redux/server/index.js
@@ -8,49 +8,18 @@ const db = mysql.createConnection({
   host: "localhost",
   user: "root",
   password: "Ze2871781O.!",
-  database: "test",
+  database: "login-example",
 });
 
 app.use(express.json());
 app.use(cors());
 
 app.get("/", (req, res) => {
-  res.json("hi from backend");
-});
-app.get("/books", (req, res) => {
-  const q = "SELECT * FROM books";
-  db.query(q, (err, data) => {
-    if (err) return res.json(err);
+  const q = "SELECT * FROM users";
+  db.query(q,(err,data)=> {
+    if(err) return res.json(err);
     return res.json(data);
-  });
-});
-
-app.post("/books", (req, res) => {
-  const q = "INSERT INTO books (`title`,`desc`,`cover`,`price`) VALUES (?)";
-  const values = [req.body.title, req.body.desc, req.body.cover, req.body.price];
-  db.query(q, [values], (err, data) => {
-    if (err) return res.json(err);
-    return res.json("Book has been created successfully");
-  });
-});
-
-app.delete("/books/:id", (req, res) => {
-  const bookId = req.params.id;
-  const q = "DELETE FROM books WHERE id = ?";
-  db.query(q, [bookId], (err, data) => {
-    if (err) return res.json(err);
-    return res.json("Book has been delete successfully.");
-  });
-});
-
-app.put("/books/:id", (req, res) => {
-  const bookId = req.params.id;
-  const q = "UPDATE books SET `title` = ?, `desc` = ?,  `cover` =?,`price` = ? WHERE id = ?";
-  const values = [req.body.title, req.body.desc, req.body.cover, req.body.price];
-  db.query(q, [...values, bookId], (err, data) => {
-    if (err) return res.json(err);
-    return res.json("Book has been update successfully.");
-  });
+  })
 });
 
 app.listen(port, () => {
--- example-redux/src/App.js
+++ example-redux/src/App.js
@@ -1,10 +1,32 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
 import "./style.css";
 
 export default function App() {
   const [changeComponent, setChangeComponent] = useState(true);
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    const FetchAllUsers = async () => {
+      try {
+        const res = await axios.get("http://localhost:8800/");
+        setUsers(res.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    FetchAllUsers();
+  }, []);
+
   return (
     <div>
+      {users.map((item)=>(
+        <div key={item.id}>
+          <p>{item.username}</p>
+          <p>{item.email}</p>
+          <p>{item.password}</p>
+        </div>
+      ))}
       {changeComponent ? (
         <div className="signin">
           <h1>Sign In</h1>
@@ -20,7 +42,7 @@ export default function App() {
           <input type="text" placeholder="username" />
           <input type="text" placeholder="email" />
           <input type="password" placeholder="password" />
-          <button onClick={()=> setChangeComponent(true)}>SIGN UP</button>
+          <button onClick={() => setChangeComponent(true)}>SIGN UP</button>
           <h2>OR</h2>
           <button onClick={() => setChangeComponent(true)}>SIGN IN NOW</button>
         </div>
