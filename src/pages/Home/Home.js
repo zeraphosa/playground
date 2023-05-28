f1fc0dcdbe78e131ca6ca16a9d3b3431033c6370
Zaur Sharifov <zaursharifov@outlook.com>
2023-05-28 12:23:37 +0400

    new mapping

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -6,39 +6,55 @@ import "./style.css";
 
 export default function Home() {
   const [data, setData] = useState(menu);
+  const [menuType, setMenuType] = useState("toyuq");
 
   return (
     <div className="home">
       <div className="buttons">
-        <button>Toyuq dönər</button>
-        <button>Ət dönər</button>
-        <button>Burger</button>
-        <button>Şorba</button>
-        <button>Salat</button>
-        <button>çİğ köftə</button>
-        <button>köftə</button>
-        <button>tost</button>
-        <button>Business lunch</button>
-        <button>İçkİ</button>
+        <button onClick={() => setMenuType("toyuq")}>Toyuq dönər</button>
+        <button onClick={() => setMenuType("et")}>Ət dönər</button>
+        <button onClick={() => setMenuType("burger")}>Burger</button>
+        <button onClick={() => setMenuType("sorba")}>Şorba</button>
+        <button onClick={() => setMenuType("salat")}>Salat</button>
+        <button onClick={() => setMenuType("cigkofte")}>çİğ köftə</button>
+        <button onClick={() => setMenuType("kofte")}>köftə</button>
+        <button onClick={() => setMenuType("tost")}>tost</button>
+        <button onClick={() => setMenuType("lunch")}>Business lunch</button>
+        <button onClick={() => setMenuType("icki")}>İçkİ</button>
       </div>
-      <div className="menu">Menu</div>
+      <div className="menu">
+        <h1>{menuType}</h1>
+        {data.map(
+          (item) =>
+            item.type === menuType && (
+              <div key={item.id}>
+                <div className="menu-info">
+                  <div>{item.name}</div>
+                  <div>{item.price}</div>
+                  <div className="price-info">
+                    <span className="count-btn">-</span>
+                    <span>{item.count}</span>
+                    <span className="count-btn">+</span>
+                  </div>
+                </div>
+              </div>
+            ),
+        )}
+      </div>
       <div className="check">Check</div>
     </div>
-
-
-
-
-    // <div className="home">
-    //   <div className="menu">
-    //     <div className="menu-item">
-    //   <Left data={data} setData={setData} />
-    //     </div>
-    //     <div className="menu-item">
-    //       <Others data={data} setData={setData} />
-    //     </div>
-    //   </div>
-    //   <div className="info">Paket</div>
-    //   <div className="done">Sifarişi tamamla</div>
-    // </div>
   );
 }
+
+// <div className="home">
+//   <div className="menu">
+//     <div className="menu-item">
+//   <Left data={data} setData={setData} />
+//     </div>
+//     <div className="menu-item">
+//       <Others data={data} setData={setData} />
+//     </div>
+//   </div>
+//   <div className="info">Paket</div>
+//   <div className="done">Sifarişi tamamla</div>
+// </div>
