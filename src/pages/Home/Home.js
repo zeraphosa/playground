333165a69108d7af51feb22b46b6409f7828cef3
Zaur Sharifov <zaursharifov@outlook.com>
2023-05-28 11:22:57 +0400

    others style done

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,7 +1,8 @@
 import { useState } from "react";
 import Donerler from "../../components/Donerler";
-import "./style.css";
+import Others from "../../components/Others";
 import menu from "../../menu";
+import "./style.css";
 
 export default function Home() {
   const [data, setData] = useState(menu);
@@ -23,115 +24,5 @@ export default function Home() {
       <div className="info">Paket, Çöl, Normal</div>
       <div className="done">Sifarişi tamamla</div>
     </div>
-  );
-}
-
-function Others({ data, setData }) {
-  function decreaseCount() {}
-  function increaseCount() {}
-
-  return (
-    <>
-      <div className="others">
-        <h1>Burger</h1>
-        {data.map((item) => {
-          if (item.type === "burger") {
-            return (
-              <div key={item.id}>
-                <div className="menu-info">
-                  <div>{item.name}</div>
-                  <div>{item.price}</div>
-                  <div className="price-info">
-                    <span className="count-btn" onClick={() => decreaseCount(item.id, "toyuq")}>
-                      -
-                    </span>
-                    <span>{item.count}</span>
-                    <span className="count-btn" onClick={() => increaseCount(item.id, "toyuq")}>
-                      +
-                    </span>
-                  </div>
-                </div>
-              </div>
-            );
-          }
-          return null;
-        })}
-      </div>
-      <div className="others">
-        <h1>Şorba</h1>
-        {data.map((item) => {
-          if (item.type === "sorba") {
-            return (
-              <div key={item.id}>
-                <div className="menu-info">
-                  <div>{item.name}</div>
-                  <div>{item.price}</div>
-                  <div className="price-info">
-                    <span className="count-btn" onClick={() => decreaseCount(item.id, "toyuq")}>
-                      -
-                    </span>
-                    <span>{item.count}</span>
-                    <span className="count-btn" onClick={() => increaseCount(item.id, "toyuq")}>
-                      +
-                    </span>
-                  </div>
-                </div>
-              </div>
-            );
-          }
-          return null;
-        })}
-      </div>
-      <div className="others">
-        <h1>Salat</h1>
-        {data.map((item) => {
-          if (item.type === "salat") {
-            return (
-              <div key={item.id}>
-                <div className="menu-info">
-                  <div>{item.name}</div>
-                  <div>{item.price}</div>
-                  <div className="price-info">
-                    <span className="count-btn" onClick={() => decreaseCount(item.id, "toyuq")}>
-                      -
-                    </span>
-                    <span>{item.count}</span>
-                    <span className="count-btn" onClick={() => increaseCount(item.id, "toyuq")}>
-                      +
-                    </span>
-                  </div>
-                </div>
-              </div>
-            );
-          }
-          return null;
-        })}
-      </div>
-      <div className="others">
-        <h1>Çiğ köftə</h1>
-        {data.map((item) => {
-          if (item.type === "cigkofte") {
-            return (
-              <div key={item.id}>
-                <div className="menu-info">
-                  <div>{item.name}</div>
-                  <div>{item.price}</div>
-                  <div className="price-info">
-                    <span className="count-btn" onClick={() => decreaseCount(item.id, "toyuq")}>
-                      -
-                    </span>
-                    <span>{item.count}</span>
-                    <span className="count-btn" onClick={() => increaseCount(item.id, "toyuq")}>
-                      +
-                    </span>
-                  </div>
-                </div>
-              </div>
-            );
-          }
-          return null;
-        })}
-      </div>
-    </>
   );
 }
