be1511700d573946264d443b8b8a1600d9a152b7
zeraphosa <zaursharifov@outlook.com>
2023-06-04 21:59:26 +0400

    home page design done

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,12 +1,16 @@
 import Image from "next/image";
 import styles from "./footer.module.css";
 
-export default function Footer(){
-    return(
-        <div className={styles.container}>
-            <div>©2023 Zeragency. All rights reserved.</div>
-            <div>
-            </div>
-        </div>
-    )
+export default function Footer() {
+  return (
+    <div className={styles.container}>
+      <div>©2023 Zeragency. All rights reserved.</div>
+      <div className={styles.social}>
+        <Image src="/1.png" width={15} height={15} className={styles.icon} alt="zeragency facebook" />
+        <Image src="/2.png" width={15} height={15} className={styles.icon} alt="zeragency instagram" />
+        <Image src="/3.png" width={15} height={15} className={styles.icon} alt="zeragency twitter" />
+        <Image src="/4.png" width={15} height={15} className={styles.icon} alt="zeragency youtube" />
+      </div>
+    </div>
+  );
 }
