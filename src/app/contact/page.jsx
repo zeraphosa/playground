9997e6cbffdc00a1cac50d403762cdceeab0d550
zeraphosa <zaursharifov@outlook.com>
2023-06-05 10:44:53 +0400

    contact page done

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,5 +1,22 @@
+import Image from "next/image";
 import styles from "./page.module.css";
+import Button from "@/components/Button/Button";
 
 export default function Contact() {
-  return <div className={styles.container}>Contact</div>;
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>Let`s Keep in Touch</h1>
+      <div className={styles.content}>
+        <div className={styles.imgContainer}>
+          <Image src="/contact.png" alt="contact img" fill={true} className={styles.img} />
+        </div>
+        <form className={styles.form}>
+          <input type="text" placeholder="name" className={styles.input}/>
+          <input type="text" placeholder="email" className={styles.input}/>
+          <textarea className={styles.textArea} placeholder="message" cols="30" rows="10"></textarea>
+          <Button url="#" text="Send"/>
+        </form>
+      </div>
+    </div>
+  );
 }
