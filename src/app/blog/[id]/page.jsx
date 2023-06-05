e09075ccf65bbc7258b5ede9be13d6dc615b0186
zeraphosa <zaursharifov@outlook.com>
2023-06-05 11:24:49 +0400

    blog post page

--- src/app/blog/[id]/page.jsx
+++ src/app/blog/[id]/page.jsx
@@ -1,3 +1,35 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
 export default function BlogPost() {
-  return <div>blog post</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.top}>
+        <div className={styles.info}>
+          <h1 className={styles.title}>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</h1>
+          <p className={styles.desc}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae facilis aliquid deleniti. Excepturi et beatae corporis possimus dolores aliquid doloribus facilis, aperiam voluptates, deleniti, ea delectus tempore sunt sit dolore!</p>
+          <div className={styles.author}>
+            <Image src="https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="author" width={40} height={40} className={styles.avatar} />
+            <span className={styles.username}>Julia Hennie</span>
+          </div>
+        </div>
+        <div className={styles.imgContainer}>
+          <Image src="https://images.pexels.com/photos/2093323/pexels-photo-2093323.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="post img" fill={true} className={styles.img} />
+        </div>
+      </div>
+      <div className={styles.content}>
+        <p className={styles.text}>
+          Lorem, ipsum dolor sit Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quos velit dolorum repellat maiores perspiciatis sapiente animi quam saepe odit molestiae, fugiat labore nesciunt alias quidem. Aspernatur sit minima minus iste! amet consectetur adipisicing elit. Optio, totam iure? Facere vero delectus ex,
+          obcaecati voluptatibus itaque officiis voluptates repellendus omnis quam nesciunt fuga eveniet, rem nam sed quos.
+          <br />
+          <br />
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reprehenderit accusantium temporibus est magni quis eaque minima, qui veniam officia. Provident accusamus quis in. Aut reprehenderit sequi voluptatibus laborum labore dignissimos. Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit, perferendis?
+          Consequatur eum animi quis necessitatibus corrupti nostrum sit ipsum, quia voluptates, doloremque est repudiandae dolore voluptas praesentium, aut quo velit.
+          <br />
+          <br />
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed rem itaque cum, odit nulla recusandae totam! Nobis consectetur cumque quibusdam natus temporibus repudiandae minus quisquam quia labore, dignissimos recusandae magnam?
+        </p>
+      </div>
+    </div>
+  );
 }
--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,7 +1,37 @@
+import Link from "next/link";
+import Image from "next/image";
 import styles from "./page.module.css";
 
 export default function Blog(){
     return(
-        <div className={styles.container}>Blog</div>
+        <div className={styles.mainContainer}>
+            <Link href="/blog/testId" className={styles.container}>
+                <div className={styles.imgContainer}>
+                    <Image src="https://images.pexels.com/photos/16353919/pexels-photo-16353919/free-photo-of-fontanna-di-trevi-in-rome-italy.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load" alt="blog img" width={400} height={250} className={styles.img}/>
+                </div>
+                <div className={styles.content}>
+                    <h1 className={styles.title}>Test</h1>
+                    <p className={styles.desc}>Desc</p>
+                </div>
+            </Link>
+            <Link href="/blog/testId" className={styles.container}>
+                <div className={styles.imgContainer}>
+                    <Image src="https://images.pexels.com/photos/16353919/pexels-photo-16353919/free-photo-of-fontanna-di-trevi-in-rome-italy.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load" alt="blog img" width={400} height={250} className={styles.img}/>
+                </div>
+                <div className={styles.content}>
+                    <h1 className={styles.title}>Test</h1>
+                    <p className={styles.desc}>Desc</p>
+                </div>
+            </Link>
+            <Link href="/blog/testId" className={styles.container}>
+                <div className={styles.imgContainer}>
+                    <Image src="https://images.pexels.com/photos/16353919/pexels-photo-16353919/free-photo-of-fontanna-di-trevi-in-rome-italy.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load" alt="blog img" width={400} height={250} className={styles.img}/>
+                </div>
+                <div className={styles.content}>
+                    <h1 className={styles.title}>Test</h1>
+                    <p className={styles.desc}>Desc</p>
+                </div>
+            </Link>
+        </div>
     )
 }
