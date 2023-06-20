182967539849e8bc6523de1164aa80ec2f4b7825
zeraphosa <zaursharifov@outlook.com>
2023-06-20 12:31:09 +0400

    swr data fetch

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,7 +1,27 @@
+"use client";
 import styles from "./page.module.css";
+import useSWR from "swr";
 
-export default function Dashboard(){
-    return(
-        <div className={styles.container}>Dashboard</div>
-    )
+export default function Dashboard() {
+  //   const [data, setData] = useState([]);
+  //   const [err, setErr] = useState(false);
+  //   const [isLoading, setIsLoading] = useState(false);
+
+  //   useEffect(() => {
+  //     async function getData() {
+  //       setIsLoading(true);
+  //       const res = await fetch("https://jsonplaceholder.typicode.com/posts", { cache: "no-store" });
+  //       if (!res.ok) {
+  //         setErr(true);
+  //       }
+  //       const data = await res.json();
+  //       setData(data);
+  //       setIsLoading(false);
+  //     }
+  //     getData();
+  //   }, []);
+  const fetcher = (...args) => fetch(...args).then((res) => res.json());
+  const { data, error, isLoading } = useSWR("https://jsonplaceholder.typicode.com/posts", fetcher);
+  console.log(data)
+  return <div className={styles.container}>Dashboard</div>;
 }
