54a9856e51a969b3f072005fed7dbe33b92bb954
zeraphosa <zaursharifov@outlook.com>
2023-07-20 14:05:46 +0400

    delete

--- src/components/Footer.js
+++
@@ -1,9 +0,0 @@
-import Player from "./Footer/Player";
-
-export default function Footer() {
-  return (
-    <div className="h-24 bg-footer border-t border-white border-opacity-5">
-      <Player />
-    </div>
-  );
-}
