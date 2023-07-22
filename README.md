ae0d050c2b507f0e15781f6a9796c268ef7a6313
zeraphosa <zaursharifov@outlook.com>
2023-07-22 10:42:56 +0400

    delete cvmaker

--- README.md
+++
@@ -1,5 +0,0 @@
-# cvmakerlab.com
-Lots of template for your CV.
-
-10 free template for free
-50 cent per template for others or monthly subscription for 2$ and reach all templates
--- src/stores/index.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import playerReducer from "./player";
-
-export default configureStore({
-  reducer: {
-    player: playerReducer,
-  },
-});
