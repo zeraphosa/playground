8c8f4e04e3e3d4aa2b6dfe0d36521db2bebbafa9
zeraphosa <zaursharifov@outlook.com>
2023-08-05 14:55:03 +0400

    slider script

--- assets/script.js
+++ assets/script.js
@@ -1,63 +1,91 @@
-const menubtn = document.getElementById("menubtn");
-const menu = document.getElementById("menu");
-const closebtn = document.getElementById("closebtn");
+const slider = document.getElementById("slider");
+const slide1 = document.getElementById("slide1");
+const slide2 = document.getElementById("slide2");
+const slide3 = document.getElementById("slide3");
 
-menubtn.addEventListener("click", () => {
-  menu.classList.toggle("active");
-  menubtn.classList.toggle("closeicon");
-  menubtn.className == "menuicon"
-    ? (menubtn.innerHTML = `
-    <i class="fa-solid fa-bars"></i>
-    `)
-    : (menubtn.innerHTML = `
-    <i class="fa-solid fa-xmark"></i>
- `);
-});
+let x = 1;
 
-var autoslider = autoslider || {
-  el: {
-    slides: document.getElementsByClassName("slide"),
-  },
+function changeSlide() {
+  x++;
+  if (x == 4) x = 1;
+  slider.style.backgroundImage = `url('./assets/img/slide${x}.jpg')`;
+  if (x == slide1.getAttribute("data-slide")) {
+    slide1.style.display = "flex";
+    slide2.style.display = "none";
+    slide3.style.display = "none";
+  } else if (x == slide2.getAttribute("data-slide")) {
+    slide2.style.display = "flex";
+    slide1.style.display = "none";
+    slide3.style.display = "none";
+  } else if (x == slide3.getAttribute("data-slide")) {
+    slide3.style.display = "flex";
+    slide2.style.display = "none";
+    slide1.style.display = "none";
+  }
+}
 
-  defaultDuration: 2000,
+setInterval(changeSlide, 4000);
 
-  init: function () {
-    console.log("ini");
+// const menubtn = document.getElementById("menubtn");
+// const menu = document.getElementById("menu");
+// const closebtn = document.getElementById("closebtn");
 
-    this.current = 0;
-    this.max = this.el.slides.length - 1;
-    this.timer = this.getTimerValue(this.current);
-    this.next();
-  },
+// menubtn.addEventListener("click", () => {
+//   menu.classList.toggle("active");
+//   menubtn.classList.toggle("closeicon");
+//   menubtn.className == "menuicon"
+//     ? (menubtn.innerHTML = `
+//     <i class="fa-solid fa-bars"></i>
+//     `)
+//     : (menubtn.innerHTML = `
+//     <i class="fa-solid fa-xmark"></i>
+//  `);
+// });
 
-  next: function () {
-    var that = this,
-      nextIndex = this.current + 1;
+// var autoslider = autoslider || {
+//   el: {
+//     slides: document.getElementsByClassName("slide"),
+//   },
 
-    if (this.current === this.max) {
-      nextIndex = 0;
-    }
+//   defaultDuration: 2000,
 
-    this.timer = this.getTimerValue(this.current);
+//   init: function () {
+//     console.log("ini");
 
-    setTimeout(
-      function () {
-        that.changeSlide(nextIndex);
-        that.current = nextIndex;
-        that.next();
-      },
-      this.timer,
-      nextIndex,
-    );
-  },
+//     this.current = 0;
+//     this.max = this.el.slides.length - 1;
+//     this.timer = this.getTimerValue(this.current);
+//     this.next();
+//   },
 
-  changeSlide: function (nextIndex) {
-    this.el.slides[this.current].classList.remove("active");
-    this.el.slides[nextIndex].classList.add("active");
-  },
+//   next: function () {
+//     var that = this,
+//       nextIndex = this.current + 1;
 
-  getTimerValue: function (index) {
-    return this.el.slides[index].dataset.duration || this.defaultDuration;
-  },
-};
-autoslider.init();
+//     if (this.current === this.max) {
+//       nextIndex = 0;
+//     }
+
+//     this.timer = this.getTimerValue(this.current);
+
+//     setTimeout(
+//       function () {
+//         that.changeSlide(nextIndex);
+//         that.current = nextIndex;
+//         that.next();
+//       },
+//       this.timer,
+//       nextIndex,
+//     );
+//   },
+
+//   changeSlide: function (nextIndex) {
+//     this.el.slides[this.current].classList.remove("active");
+//     this.el.slides[nextIndex].classList.add("active");
+//   },
+
+//   getTimerValue: function (index) {
+//     return this.el.slides[index].dataset.duration || this.defaultDuration;
+//   },
+// };
+// autoslider.init();
