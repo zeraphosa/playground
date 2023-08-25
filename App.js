bc16a9e3c04e6b6b83052ff3acdab153ef7c605a
zeraphosa <zaursharifov@outlook.com>
2023-08-25 14:11:07 +0400

    game world

--- App.js
+++ App.js
@@ -1,20 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { View } from "react-native";
+import { GameEngine } from "react-native-game-engine";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start sad working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1 }}>
+      <GameEngine style={{ position: "absolute", top: 0, left: 0, right: 0, bottom: 0 }}>
+        
+      </GameEngine>
+
+      <StatusBar style="auto" hidden={true} />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ entities/index.js
@@ -0,0 +1,10 @@
+import Matter from "matter-js";
+
+export default (restart) => {
+  let engine = Matter.Engine.create({ enableSleeping: false });
+  let world = engine.world;
+  world.gravity.y = 0.4;
+  return {
+    physics: { engine, world },
+  };
+};
