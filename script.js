6052ea61e60d6baae4da1eef36115b3e29c50ddb
zeraphosa <zaursharifov@outlook.com>
2023-09-14 21:48:47 +0400

    autoClose add

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-import Toast from "./Toast.js";
+import Toast from "./toast.js";
 
-const myToast = new Toast("Hello world. Hope this message find you well.", "", true, false, 2000, 5, "green", "#fff", "green");
+const myToast = new Toast("Hello world. Hope this message find you well.", "", false, 2000, 5, "green", "#fff", "green");
 myToast.show();
--- toast.js
+++ toast.js
@@ -1,8 +1,7 @@
 class Toast {
-  constructor(text, position, closeble, autoClose, duration, borderWidth, borderColor, textColor, bgColor) {
+  constructor(text, position, autoClose, duration, borderWidth, borderColor, textColor, bgColor) {
     this.text = text;
     this.position = position;
-    this.closeble = closeble;
     this.autoClose = autoClose;
     this.duration = duration;
     this.borderColor = borderColor;
@@ -17,6 +16,7 @@ class Toast {
             <span>
                 <svg id="icon" fill='${this.textColor}' xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 48 48" width="48px" height="48px"><path d="M 38.982422 6.9707031 A 2.0002 2.0002 0 0 0 37.585938 7.5859375 L 24 21.171875 L 10.414062 7.5859375 A 2.0002 2.0002 0 0 0 8.9785156 6.9804688 A 2.0002 2.0002 0 0 0 7.5859375 10.414062 L 21.171875 24 L 7.5859375 37.585938 A 2.0002 2.0002 0 1 0 10.414062 40.414062 L 24 26.828125 L 37.585938 40.414062 A 2.0002 2.0002 0 1 0 40.414062 37.585938 L 26.828125 24 L 40.414062 10.414062 A 2.0002 2.0002 0 0 0 38.982422 6.9707031 z"/></svg>
             </span>
+            <div id="line"></div>
         </div>
     `;
     const toast = document.getElementById("toast");
@@ -58,11 +58,15 @@ class Toast {
     icon.style.width = "16px";
     icon.style.height = "16px";
     icon.style.cursor = "pointer";
-    icon.addEventListener("click", () => this.close());
-    toast.addEventListener("click", () => this.close());
-  }
-  close() {
-    console.log("closed");
+    toast.addEventListener("click", () => autoCloseFunc());
+
+    if (this.autoClose === true) {
+      setTimeout(autoCloseFunc, this.duration);
+    }
+    function autoCloseFunc() {
+      document.body.remove(icon);
+      document.body.remove(toast);
+    }
   }
 }
 export default Toast;
