d461ca7bf05983ddf42e5fe3115de415eb9abc79
zeraphosa <zaursharifov@outlook.com>
2023-10-09 11:53:48 +0400

    slider thumbs & arrows

--- js-slider/script.js
+++ js-slider/script.js
@@ -1,6 +1,16 @@
 const slider = document.getElementById("slider");
 const images = ["ada.jpg", "alma.jpg", "autumn.jpg", "eynek.jpg", "ocean.jpg", "plaj.jpg", "sepit.jpg", "xezer.jpg"];
 let x = 0;
+let thumbs = document.createElement("div");
+thumbs.classList.add("thumbs");
+slider.appendChild(thumbs);
+
+for (let i = 0; i < images.length - 1; i++) {
+  let thumb = document.createElement("img");
+  thumb.classList.add("thumb");
+  thumb.setAttribute('src',`./img/${images[i]}`)
+  thumbs.appendChild(thumb);
+}
 
 slider.addEventListener("click", (e) => {
   console.log(e.pageX);
@@ -9,13 +19,12 @@ slider.addEventListener("click", (e) => {
 });
 
 let timer = setTimeout(change, 0, 1);
-change(1);
+
 function change(status) {
   clearTimeout(timer);
   x += status;
   if (x > images.length - 1) x = 0;
   else if (x < 0) x = images.length - 1;
   slider.style.backgroundImage = `url('./img/${images[x]}')`;
-
   timer = setTimeout(change, 3000, 1);
 }
