fcb125dd8ca6efad6db68b798024b586084351f3
zeraphosa <zaursharifov@outlook.com>
2023-10-09 14:05:56 +0400

    thumbs change to dots

--- js-slider/script.js
+++ js-slider/script.js
@@ -4,11 +4,12 @@ let x = 0;
 let thumbs = document.createElement("div");
 thumbs.classList.add("thumbs");
 slider.appendChild(thumbs);
+let timer = setTimeout(change, 0, 1);
 
 for (let i = 0; i < images.length; i++) {
   let thumb = document.createElement("img");
   thumb.classList.add("thumb");
-  thumb.setAttribute("src", `./img/${images[i]}`);
+  // thumb.setAttribute("src", `./img/${images[i]}`);
   thumb.addEventListener("click", (e) => {
     thumbs.children[x].classList.remove("thumb-active");
     e.stopPropagation();
@@ -24,17 +25,15 @@ slider.addEventListener("click", (e) => {
   else change(1);
 });
 
-let timer = setTimeout(change, 0, 1);
-
 function change(y) {
   clearTimeout(timer);
-  let smt = x;
+  let oldSelection = x;
   x += y;
   if (x > images.length - 1) x = 0;
   else if (x < 0) x = images.length - 1;
   slider.style.backgroundImage = `url('./img/${images[x]}')`;
   timer = setTimeout(change, 3000, 1);
 
-  thumbs.children[smt].classList.remove("thumb-active");
+  thumbs.children[oldSelection].classList.remove("thumb-active");
   thumbs.children[x].classList.add("thumb-active");
 }
